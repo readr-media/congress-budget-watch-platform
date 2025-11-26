1adf2b31aab27e7e4c37a9c5c467ccd5477a17cb
LinHeMa <109965534+LinHeMa@users.noreply.github.com>
2025-11-26 15:24:55 +0800

    chore(graphql): Regenerate types after updating proposal query

--- app/queries/proposal.queries.ts
+++ app/queries/proposal.queries.ts
@@ -65,11 +65,11 @@ export const GET_PROPOSAL_BY_ID_QUERY = graphql(`
       react_good
       react_whatever
       budgetImageUrl
-      historicalProposals {
+      historicalParentProposals {
         id
-        meetings {
-          id
-        }
+      }
+      mergedParentProposals {
+        id
       }
       government {
         id
@@ -120,6 +120,9 @@ export const GET_PROPOSAL_BY_ID_QUERY = graphql(`
       }
       historicalProposals {
         id
+        meetings {
+          id
+        }
         proposers {
           id
           name
